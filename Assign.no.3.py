5e7e012c139bcd32003ffa791f4bad7e6e814d3f
kusuraj-08 <63755012+kusuraj-08@users.noreply.github.com>
2020-05-20 12:43:45 +0530

    Add files via upload

---
+++ Assign.no.3.py
@@ -0,0 +1,13 @@
+#list1
+list1=[12,-7,5,64,-14]
+print("List1:")
+for i in list1:
+    if(i>=0):
+        print(i,end=" ")
+print('\n')
+#list2    
+list2=[12,14,-95,3]
+print("List2:")
+for i in list2:
+    if(i>=0):
+        print(i,end=" ")
